7b8d352bf5eec54a539ae9482ca9d76b7c666210
joseph.guidry <joseph.guidry4@gmail.com>
2017-05-11 18:52:43 -0400

    Updated check_number() and added myatoi()

--- timestable.c
+++ timestable.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 
-void print_table(int x, int y);
+#define MAX 20
+
+void print_table(int x, int y, char format[]);
+int check_number(char arg[]);
+int myatoi(char arg[]);
+void table_format(int min, int max, char format[]);
 
 int main(int argc, char *argv[])
 {
-	int start, end;
+	int start = 1, end = 10;
 	if (argc == 2){
-		end = atoi(argv[1]);
-	}
-    if (argc < 2){
-    	start = 1; end = 10;
-    	print_table(start, end);
+		if(check_number(argv[1]) == 0){
+			end = myatoi(argv[1]);
+		} else {
+			printf("Arguement is not a number");
+			return 0;
+		}
+	}	
+	if (argc < 2){
+    	char string[MAX] = "%3d "; 
+    	print_table(start, end, string);
     }
 }
 
-void print_table(int min, int max)
+void print_table(int min, int max, char format[])
+//Takes the start and end values, with Formatting. Prints times tables.
 {
-	printf("%d %d\n", min, max);
-	for (min; min <= max; min++){		
+	for(min; min <= max; min++){		
 		for(int x = 1; x <= max; x++){
-			printf(" %3d", x * min);
+			printf(format, x * min);
 		}
 		printf("\n");
 	}
+}
+
+int check_number(char arg[])
+//Take in first arguement.  Return integer. 
+{
+	for(int i = 0; arg[i] != '\0' ; i++){
+		if ((arg[i] < '0') || (arg[i] > '9')){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int myatoi(char arg[])
+//Take arguement value. Return integer value. 
+{
+	//check if character array is all numbers.
+	//if yes, convert to integer
+	//return arg
+	printf("In myatoi Function");
+	return 1;
 }
